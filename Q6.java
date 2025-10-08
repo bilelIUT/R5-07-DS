6501a1d9fdc2c71f1a05940d21c59a492431531b
bilelIUT <bilelchriki2811@gmail.com>
2025-10-08 17:00:04 +0200

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -4,7 +4,7 @@ public class Q6 {
             System.out.println("Usage : java Q5 <N>");
             return;
         }
-        
+    
         int N = Integer.parseInt(args[0]);
         for (int i = N; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
