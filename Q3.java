8e418d795cfb14d384fb473a465e56ad1bcc84cc
bilelIUT <bilelchriki2811@gmail.com>
2025-10-08 14:10:29 +0000

    push java

--- Q3.java
+++ Q3.java
@@ -1,1 +1,11 @@
+public class Q3 {
+    public Q3(int n) {
+        for(int i=0;i<=n;i++){
+            System.out.println(i);
+        }
+    }
 
+    public static void main(String[] a){
+        new Q3(10);
+    }
+}
